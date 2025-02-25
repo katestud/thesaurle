846c20a5ddbb1d313cf9c51d601e489c2aee19ec
Kate Studwell <katestudwell@gmail.com>
2025-02-25 15:29:22 -0500

    Semi working game with some bugs

---
+++ game.py
@@ -0,0 +1,68 @@
+import wordfreq
+import random
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_synonyms_api_ninjas(word):
+  url = "https://api.api-ninjas.com/v1/thesaurus?word={}".format(word)
+  response = requests.get(url, headers={'X-Api-Key': os.getenv('API_NINJAS_KEY')})
+  if response.status_code == requests.codes.ok:
+    return response.json()["synonyms"]
+  else:
+    return None
+
+def get_synonyms_subset(word, corpus):
+  results = get_synonyms_api_ninjas(word)
+  return list(set(corpus).intersection(set(results)))
+
+def choose_synonym(list):
+  return random.choice(list)
+
+def game_setup(start_word, length):
+  path = []
+  current = start_word
+
+  for i in range(length):
+    synonyms = get_synonyms_subset(current, common_words)
+    current = choose_synonym(synonyms)
+    path.append(current)
+
+  return path
+
+def take_turn(target_word, guess_options, corpus):
+    synonyms = []
+    while len(synonyms) == 0:
+        guess = input("Enter your guess: ")
+        if not (guess in guess_options):
+            print("Not a valid guess")
+            continue
+        if guess == target_word:
+            print("You won")
+            return []
+        synonyms = get_synonyms_subset(guess, corpus)
+        if len(synonyms) == 0:
+            print("Sorry, that word has no synonyms")
+    return synonyms
+
+def play_the_game(starting_word, end_word, num_turns, corpus):
+    print(f"You can get from {starting_word} to {end_word} in {num_turns} steps")
+    synonyms = get_synonyms_subset(starting_word, corpus)
+    for i in range(num_turns):
+        print(synonyms)
+        synonyms = take_turn(end_word, synonyms, corpus)
+        if len(synonyms) == 0:
+            print("Hooray")
+            return
+    print("Sorry, you lost!")
+
+common_words = wordfreq.top_n_list('en', 5000)
+max_length = 5
+start_word = "stand"
+
+path = game_setup(start_word, max_length)
+end_word = path[-1]
+
+play_the_game(start_word, end_word, max_length, common_words)
