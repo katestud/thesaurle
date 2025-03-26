2230eef2cceb47b22134f67f491e048768963ee1
Kate Studwell <katestudwell@gmail.com>
2025-03-26 17:03:29 -0400

    Format the game file

--- game.py
+++ game.py
@@ -5,80 +5,87 @@ conn = mgclient.connect(host="127.0.0.1", port=7687)
 cursor = conn.cursor()
 
 def fetch_shortest_path(start, end, max_hops=10):
-  query = f"""
+    query = f"""
   MATCH path=(n:Word {{name: $start_word}})-[relationships:SYNONYM *BFS ..{max_hops}]->(m:Word {{name: $end_word}})
   RETURN nodes(path);
   """
-  cursor.execute(query, {'start_word': start, 'end_word': end})
-  path = []
-  for row in cursor.fetchall():
-    for item in row[0]:
-      path.append(item.properties['name'])
+    cursor.execute(query, {'start_word': start, 'end_word': end})
+    path = []
+    for row in cursor.fetchall():
+        for item in row[0]:
+            path.append(item.properties['name'])
 
-  return path
+    return path
+
 
 def get_synonyms(word):
-  query = """
+    query = """
   MATCH (a:Word {name: $word})-[r:SYNONYM]->(b:Word)
   RETURN b
   """
-  cursor.execute(query, {"word": word})
-  results = cursor.fetchall()
+    cursor.execute(query, {"word": word})
+    results = cursor.fetchall()
 
-  syns = []
-  for row in results:
-      name = row[0].properties['name']
-      syns.append(name)
+    syns = []
+    for row in results:
+        name = row[0].properties['name']
+        syns.append(name)
 
-  return syns
+    return syns
+
 
 def game_setup():
-  lines = open("data/possible_pairs.txt").read().splitlines()
-  start,end,_ =random.choice(lines).split(",")
+    lines = open("data/possible_pairs.txt").read().splitlines()
+    start, end, _ = random.choice(lines).split(",")
 
-  return fetch_shortest_path(start, end)
+    return fetch_shortest_path(start, end)
+
 
 def play_the_game(path, num_turns):
-  start_word = path[0]
-  target_word = path[-1]
-  dist_to_target = len(path) - 1
+    start_word = path[0]
+    target_word = path[-1]
+    dist_to_target = len(path) - 1
 
-  print(f"You can get from {start_word} to {target_word} in {dist_to_target} steps")
-  guess_options = get_synonyms(start_word)
+    print(
+        f"You can get from {start_word} to {target_word} in {dist_to_target} steps")
+    guess_options = get_synonyms(start_word)
 
-  turns_taken = 0
-  game_won = False
-  taken_guesses = []
+    turns_taken = 0
+    game_won = False
+    taken_guesses = []
 
-  factor = 2
+    factor = 2
 
-  while not game_won and turns_taken < factor * num_turns:
-    turns_taken = turns_taken + 1
-    for index, o in enumerate(guess_options):
-      print(f"{index}: {o}")
+    while not game_won and turns_taken < factor * num_turns:
+        turns_taken = turns_taken + 1
+        for index, o in enumerate(guess_options):
+            print(f"{index}: {o}")
 
-    try:
-      guess_index = input("enter your guess: ")
-      # TODO: Validate the input
-      guess = guess_options[int(guess_index)]
-      print(f"You chose: {guess}")
-    except KeyboardInterrupt:
-      print("quitting game")
-      break
+        try:
+            guess_index = input("enter your guess: ")
+            # TODO: Validate the input
+            guess = guess_options[int(guess_index)]
+            print(f"You chose: {guess}")
+        except KeyboardInterrupt:
+            print("quitting game")
+            break
 
-    if not (guess in guess_options):
-      print("not a valid guess")
-    else:
-      if guess == target_word:
-        game_won = True
-      else:
-        taken_guesses.append(guess)
-        dist_to_target = len(fetch_shortest_path(guess, target_word)) - 1
-        print(f"Guesses so far: {taken_guesses}. Try to get to {target_word}. You are at least {dist_to_target} steps away")
-        guess_options = get_synonyms(guess)
+        if not (guess in guess_options):
+            print("not a valid guess")
+        else:
+            if guess == target_word:
+                game_won = True
+            else:
+                taken_guesses.append(guess)
+                dist_to_target = len(
+                    fetch_shortest_path(guess, target_word)) - 1
+                print(
+                    f"Guesses so far: {taken_guesses}. Try to get to {target_word}. You are at least {dist_to_target} steps away")
+                guess_options = get_synonyms(guess)
 
-  if(game_won):
-    print("🎉🎉🎉 Hooray 🎉🎉🎉!")
+    if (game_won):
+        print("🎉🎉🎉 Hooray 🎉🎉🎉!")
+
 
 print("Initializing game play!")
 path = game_setup()
